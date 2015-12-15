38885d8b05b2be3d604cc1d7ceff7910067a085d
amy <ayyshi@gmail.com>
2015-12-14 22:42:12 -0500

    events returned by date and time of event, changes to even params

--- api/controllers/eventsController.js
+++ api/controllers/eventsController.js
@@ -2,19 +2,43 @@
 
 const request    = require('request');
 const bodyParser = require('body-parser');
+const dateFormat = require('dateformat');
 const Event      = require('../models/event');
 
 // show all events
 function getAll(req, res){
   Event.find((err, events) => {
-    // returns events in decending order of startDate
-    res.send(events.sort({startDate: -1}));
+    // returns events in decending order of startDate and startTime
+    res.send(events.sort({startDate: -1, startTime: -1}));
   });
 };
 
 // POST new event
 function newEvent(req, res){
-  let eventObj = new Event(req.body);
+  // convert dates from UTC
+  let startDate = dateFormat(req.body.startDate, "mmm dd, yyyy");
+  let endDate = dateFormat(req.body.endDate, "mmm dd, yyyy");
+  // convert times from UTC
+  let startTime = dateFormat(req.body.startTime, "HH:MM");
+  let endTime = dateFormat(req.body.endTime, "HH:MM");
+
+  let eventParams = {
+    title:        req.body.title,
+    description:  req.body.description,
+    type:         req.body.type,
+    category:     req.body.category,
+    startDate:    req.body.startDate,
+    displayStart: startDate,
+    startTime:    startTime,
+    endDate:      req.body.endDate,
+    displayEnd:   endDate,
+    endTime:      endTime,
+    location:     req.body.location,
+    eventUrl:     req.body.eventUrl,
+    tags:         req.body.tags
+  };
+
+  let eventObj = new Event(eventParams);
 
   eventObj.save((err, book) => {
     if(err) res.status(401).send('couldn\'t save new event ' + err);
