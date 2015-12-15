533b7573ecac5a49470290130821b25aac2f5ecd
amy <ayyshi@gmail.com>
2015-12-15 14:32:14 -0500

    working backend token auth

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,52 @@
 angular
   .module('museum-events', ['ui.router'])
-  .config(EventRouter);
+  .config(EventRouter)
+  .factory('authInterceptor', function ($rootScope, $q, $window) {
+    return {
+      request: function (config) {
+      	if ($window.localStorage.getItem('userToken')) {
+      		config.headers.Authorization =
+      		  'Bearer ' + $window.localStorage.getItem('userToken');
+      	}
+        return config;
+      },
+      response: function (response) {
+        if (response.status === 401) {
+          // handle the case where the user is not authenticated
+        }
+        return response || $q.when(response);
+      }
+    };
+  })
+
+  .config(function ($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptor');
+  })
+  // .factory('User', function() {
+  //   return {
+  //     loginUser : false
+  //   };
+  // })
+  // .controller('UserController',['$scope', 'User', function($scope, User) {
+  //   $scope.user = User;
+  // }])
+  // .run(['$rootScope', '$state', 'User', function($rootScope, $state, User) {
+  //   $rootScope.$on('$stateChangeStart',
+  //   function(event, toState, toParams, fromState, fromParams) {
+  //
+  //     let isAuthenticationRequired =  toState.data
+  //           && toState.data.requiresLogin
+  //           && !User.isLoggedIn;
+  //
+  //     if(isAuthenticationRequired) {
+  //       event.preventDefault();
+  //       $state.go('login');
+  //     };
+  //   });
+  // }])
 
 function EventRouter($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/index');
+  $urlRouterProvider.otherwise('/');
 
   $stateProvider
   .state('index', {
@@ -12,7 +55,7 @@ function EventRouter($stateProvider, $urlRouterProvider) {
   })
   .state('newEvent', {
     url: '/events/new',
-    templateUrl: 'newEvent.html'
+    templateUrl: 'newEvent.html',
   })
   .state('editEvent', {
     url: '/events/edit/:id',
