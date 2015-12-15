f9cc595841dedb9139f06fe95b5633c0be6bcaf1
amy <ayyshi@gmail.com>
2015-12-14 22:42:36 -0500

    changes to events model for render

--- api/models/event.js
+++ api/models/event.js
@@ -3,17 +3,19 @@
 const mongoose = require('mongoose');
 
 let EventSchema = new mongoose.Schema({
-  title:       { type: String, required: true },
-  description: { type: String },
-  type:        { type: String },
-  category:    { type: String },
-  startDate:   { type: Date, required: true },
-  startTime:   { type: Date },
-  endDate:     { type: Date },
-  endTime:     { type: Date },
-  location:    { type: String, required: true },
-  eventUrl:    { type: String },
-  tags:        [{ type: String }]
+  title:        { type: String, required: true },
+  description:  { type: String },
+  type:         { type: String },
+  category:     { type: String },
+  startDate:    { type: Date, required: true },
+  displayStart: { type: String },
+  startTime:    { type: String },
+  endDate:      { type: Date },
+  displayEnd:   { type: String },
+  endTime:      { type: String },
+  location:     { type: String, required: true },
+  eventUrl:     { type: String },
+  tags:         [{ type: String, required: true }]
 });
 
 let Event = mongoose.model('Event', EventSchema);
