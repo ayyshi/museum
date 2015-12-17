fb223e970ce969f86c1f64157db2d78af4b34b09
amy <ayyshi@gmail.com>
2015-12-16 23:26:55 -0500

    deploy issues

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/', express.static(__dirname + '/public'));
 app.use('/user', userRoutes);
 app.use('/events', eventRoutes);
 
